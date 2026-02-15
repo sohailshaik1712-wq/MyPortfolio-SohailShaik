f7defec2593a9e5bc4640bc6081ddcecf1e948fc
sohailshaik1712-wq <sohailshaik1712@gmail.com>
2026-02-15 23:31:13 +0530

    few changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080'}/:path*`,
       },
     ];
   },
